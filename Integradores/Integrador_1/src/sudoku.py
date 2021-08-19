95d637de7ea30ec543f5e756d8cde468bb779f92
nfernandezsanz <nfernndezsanz@frba.utn.edu.ar>
2021-08-19 19:05:13 -0300

    Clase 19082021

--- Integradores/Integrador_1/src/sudoku.py
+++ Integradores/Integrador_1/src/sudoku.py
@@ -11,18 +11,22 @@ def crear_juego(representacion):
 
     La representación es una cadena con el siguiente formato:
 
-    003020600
-    900305001
-    001806400
-    008102900
-    700000008
-    006708200
-    002609500
-    800203009
-    005010300
+    003020600'\n'
+    900305001'\n'
+    001806400'\n'
+    008102900'\n'
+    700000008'\n'
+    006708200'\n'
+    002609500'\n'
+    800203009'\n'
+    005010300'\n'
 
     Donde un 0 significa que la casilla está vacía.
     '''
+
+    '''Opcion  1 (Dolorosa)  ==> Trabajo con for e ir cargando mi lista'''
+    '''Opcion  2 (Investigo) ==> Utilizar la operacion split (operacion de listas, similar a append)'''
+    
     pass
     
 def hay_valor_en_fila(sudoku, fila, valor):
